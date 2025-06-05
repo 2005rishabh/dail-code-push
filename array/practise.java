b94453fdf20f37c0eb2a40871a1c190bec7fef4f
Rishabh Singh <20rishabh05@gmail.com>
2025-06-05 23:41:25 +0530

    two sum question

---
+++ array/practise.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class practise {
+    public static void main(String[] args) {
+        String s = "flow";
+        String t = "flower";
+        System.out.println(ts);
+    }
+}
