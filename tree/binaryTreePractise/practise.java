3922309cc63058a8838cc0c7fd18e9e6f3b9dc49
Rishabh Singh <20rishabh05@gmail.com>
2025-06-13 11:07:33 +0530

    Started tree

---
+++ tree/binaryTreePractise/practise.java
@@ -0,0 +1,30 @@
+package binaryTreePractise;
+
+public class practise {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }       
+    }
+
+    static class binaryTree{
+        static int x = -1;
+        public static Node createTree(int[] arr) {
+            x++;
+            if(arr[x] == -1){
+                return null;
+            }
+            Node newNode = new Node(arr[x]);
+            newNode.left = createTree(arr);
+            newNode.right = createTree(arr);
+            return newNode;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    }
+}
