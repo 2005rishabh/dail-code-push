8d40752c180fc66b1f654e371eee10dbe30a9ab9
Rishabh Singh <20rishabh05@gmail.com>
2025-06-24 11:47:02 +0530

    started graph

--- tree/binaryTreePractise/practise.java
+++ tree/binaryTreePractise/practise.java
@@ -90,6 +90,8 @@ public class practise {
         }
     }
 
+    
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         Node root = BinaryTree.createTree(arr);
